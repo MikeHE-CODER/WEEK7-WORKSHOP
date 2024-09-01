659847d36bfc167a82d6fd0f77957bf443d21ef0
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-09-01 22:49:51 +0930

    Create main-1-1.cpp

---
+++ main-1-1.cpp
@@ -0,0 +1,13 @@
+#include "Appliance.h"
+#include <iostream>
+using namespace std;
+int main() {
+  Appliance a;
+  Appliance b(5);
+  a.getison();
+  a.setPowerRating(5);
+  a.setisOn(1);
+  a.turnOn();
+  a.turnOff();
+  a.getPowerComsumption();
+}
