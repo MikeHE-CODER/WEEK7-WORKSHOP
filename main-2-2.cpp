421e115d897338aee4c21b95272677cd0b33e58a
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-09-02 10:31:18 +0930

    Update main-2-2.cpp

--- main-2-2.cpp
+++ main-2-2.cpp
@@ -6,8 +6,8 @@ using namespace std;
 int main() {
   TV(a);
   TV b(5, 6);
-  a.get_ison();
-  a.set_PowerRating(5);
+  a.get_isOn();
+  a.set_powerRating(5);
   a.set_isOn(1);
   a.turnOn();
   a.turnOff();
