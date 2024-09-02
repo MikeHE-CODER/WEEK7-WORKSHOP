0605fcc16e8e89e3af46ccb1d64fb93da18c38fa
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-09-02 10:57:49 +0930

    Update House.cpp

--- House.cpp
+++ House.cpp
@@ -4,20 +4,30 @@
 #include "TV.h"
 #include <iostream>
 using namespace std;
+
 House::House() : appliances(nullptr), numAppliances(0), number(0) {}
-House::House(int numAppliances) : appliances(new Appliance *[numAppliances])，number(0) {}
+
+House::House(int numAppliances) : appliances(new Appliance *[numAppliances]), numAppliances(numAppliances), number(0) {}
+
 bool House::addAppliance(Appliance *appliance) {
-  if (number < numAppliances) {
-    appliances[number] = appliance;
-    number++;
-    return true;
-  }
-  return false;
+    if (number < numAppliances) {
+        appliances[number] = appliance;
+        number++;
+        cout << appliance->get_PowerRating() << " watts appliance added to the house" << endl;
+        return true;
+    }
+    return false;
 }
+
 double House::getTotalPowerConsumption() {
-  double totalPowerConsumption = 0;
-  for (int i = 0; i < number; i++) {
-    totalPowerConsumption += appliances[i]->getPowerConsumption();
-  }
-  return totalPowerConsumption;
+    double totalPowerConsumption = 0;
+    for (int i = 0; i < number; i++) {
+        totalPowerConsumption += appliances[i]->getPowerConsumption();
+    }
+    return totalPowerConsumption;
 }
+
+House::~House() {
+    delete[] appliances;
+}
+
