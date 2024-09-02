d949cbd755fd5afa04ed260fae08d96575b89b4e
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-09-02 10:13:39 +0930

    Update Appliance.cpp

--- Appliance.cpp
+++ Appliance.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 Appliance::Appliance() : powerRating(0), isOn(false){};
 Appliance::Appliance(int powerRating) : powerRating(powerRating), isOn(false){};
-bool Appliance::getison() { return isOn; };
-int Appliance::getPowerRating() { return powerRating; };
-void Appliance::setPowerRating(int powerRating) {
+bool Appliance::get_ison() { return isOn; };
+int Appliance::get_PowerRating() { return powerRating; };
+void Appliance::set_PowerRating(int powerRating) {
   this->powerRating = powerRating;
 };
-void Appliance::setisOn(bool ison) { this->isOn = ison; };
+void Appliance::set_isOn(bool ison) { this->isOn = ison; };
 void Appliance::turnOn() { this->isOn = true; };
 void Appliance::turnOff() { this->isOn = false; };
 double Appliance::getPowerConsumption() { return 0; };
