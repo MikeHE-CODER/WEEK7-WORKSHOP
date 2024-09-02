5c9a7efb9df42888c57a96e90eee6070e125c3b8
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-09-02 10:14:20 +0930

    Update main-1-1.cpp

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,12 +1,13 @@
+
 #include "Appliance.h"
 #include <iostream>
 using namespace std;
 int main() {
   Appliance a;
   Appliance b(5);
-  a.getison();
-  a.setPowerRating(5);
-  a.setisOn(1);
+  a.get_ison();
+  a.set_PowerRating(5);
+  a.set_isOn(1);
   a.turnOn();
   a.turnOff();
   a.getPowerConsumption();
