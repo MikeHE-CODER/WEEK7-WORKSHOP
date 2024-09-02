8d5334b535274d348f080432c2ac74e125b57b65
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-09-02 10:10:13 +0930

    Create House.cpp

---
+++ House.cpp
@@ -0,0 +1,21 @@
+#include "House.h"
+#include "Appliance.h"
+#include <iostream>
+using namespace std;
+House::House() : Appliance(), appliances(nullptr) {}
+House::House(int numAppliances) : appliances(new Appliance *[numAppliances]) {}
+bool House::addAppliance(Appliance *appliance) {
+  if (number < numAppliances) {
+    appliances[number] = appliance;
+    number++;
+    return true;
+  }
+  return false;
+}
+double House::getTotalPowerConsumption() {
+  double totalPowerConsumption=0;
+  for (int i = 0; i < number; i++) {
+    totalPowerConsumption += appliances[i]->getPowerConsumption();
+  }
+  return totalPowerConsumption;
+}
