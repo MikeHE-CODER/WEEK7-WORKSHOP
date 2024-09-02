8ba9435a929aad9191a8ab3628e54f320ce363d9
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-09-02 10:28:52 +0930

    Update Fridge.cpp

--- Fridge.cpp
+++ Fridge.cpp
@@ -5,10 +5,10 @@ using namespace std;
 Fridge::Fridge(){};
 Fridge::Fridge(int powerRating, double volume) {
   this->volume = volume;
-  this->set_PowerRating(powerRating);
+  this->set_powerRating(powerRating);
 }
 void Fridge::setVolume(double volume) { this->volume = volume; };
 double Fridge::getVolume() { return volume; };
 double Fridge::getPowerConsumption() {
-  return this->get_PowerRating() * 24 * volume / 100;
+  return this->get_powerRating() * 24 * volume / 100;
 };
